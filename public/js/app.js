ca414030714f5fcb498d10423c4dc2d112c19d79
Ibrahimboussta <ibrahimboussta@gmail.com>
2024-01-19 14:44:03 +0100

    responsive

--- public/js/app.js
+++ public/js/app.js
@@ -16,60 +16,118 @@ login.addEventListener('click', () => {
     login_in.style.display="block"
 })
 
-
-
-
-
-
-
-
-
-
-connexion.addEventListener('click', () => {
-    modale.style.display = 'block'
-})
-close.addEventListener('click', () => {
-    modale.style.display = 'none'
-})
-
-// let BreakFast = document.getElementById('braekfast')
-// // li bghina nms7o
-// let Starter_btn = document.getElementById('btn-starter')
-// let menu_breakfast = document.querySelector('.menu_break')
-// let btn_menu_dinner = document.querySelector('.menu_dinner')
-// let menu_dinner = document.querySelector('#dinner_menu')
-// let braek_menu = document.querySelector('.breakfast-menu')
-
-// let dinner = document.getElementById('denner')
-// let braekFast = document.getElementById('BraekFast')
-// console.log(BreakFast, Starter);
-
-// menu_breakfast.addEventListener('click', () => {
-//     console.log('hihi');
-//     braek_menu.style.display = 'none'
-//     menu_dinner.style.display = 'block'
-
-//     // dinner.style.display = 'block'
-//     // Starter.style.display = 'none'
-
-//     // menu.style.display='none'
+// connexion.addEventListener('click', () => {
+//     modale.style.display = 'block'
+// })
+// close.addEventListener('click', () => {
+//     modale.style.display = 'none'
 // })
 
-let braek_btn = document.querySelector('.menu_break')
-let break_menu = document.querySelector('.breakfast-menu')
-let dinner_menu = document.getElementById('dinner_menu')
-braek_btn.addEventListener('click', () => {
-    dinner_menu.style.display = 'block'
-    break_menu.style.display = 'none'
-})
 
 
 let play_btn = document.getElementById('video')
+let btnPlay = document.getElementById('play')
 let video = document.querySelector('.video')
+let video1 = document.querySelector('.video1')
+let fermer = document.querySelector('.fermer')
+let fermer1 = document.querySelector('.fermer1')
+
 play_btn.addEventListener('click', () => {
-    document.body.style.src
+    // document.body.style.src
     video.style.display = 'block'
+    fermer.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+
     // window.TransitionEvent = '1s'
     document.body.setAttribute('class', 'blur')
     // video.style.zIndex = 999
 })
+fermer.addEventListener('click', () => {
+    video.style.display = 'none'
+    fermer.style.display = 'none'
+})
+
+btnPlay.addEventListener('click', () => {
+    // document.body.style.src
+    video1.style.display = 'block'
+    fermer1.style.display = 'block'
+    // window.TransitionEvent = '1s'
+    // document.body.setAttribute('class', 'blur')
+    // video.style.zIndex = 999
+})
+
+fermer1.addEventListener('click', () => {
+    video1.style.display = 'none'
+    fermer1.style.display = 'none'
+})
+
+
+// let img = document.querySelectorAll('#imges')
+// let prevBtn = document.getElementById('prevBtn')
+// let nextBtn = document.getElementById('nextBtn')
+// let dots = document.querySelectorAll('.dot')
+// // console.log(img);
+
+// let counter = 0
+
+// nextBtn.addEventListener('click', nextSlide)
+// function nextSlide() {
+//     img[counter].style.animation = 'next1 0.5s ease-in forwards'
+//     if (counter >= img.length - 1) {
+//         counter = 0;
+//     }else{
+//         counter++;
+//     }
+//     img[counter].style.animation = 'next2 0.5s ease-in forwards'
+
+// }
+
+// let slidInit = () => {
+//     let imgList = document.querySelectorAll('#imges')
+//     let btns = document.querySelectorAll('.prevBtn', '.nextBtn')
+
+//     btns.forEach(element => {
+//         element.addEventListener('click', () => {
+//             console.log(element);
+//             let direction = element.id === ''
+//         })
+//     });
+// }
+
+
+let images = document.querySelectorAll('.item')
+let prevBtn = document.getElementById('prevBtn')
+let nextBtn = document.getElementById('nextBtn')
+let counter = 0
+let counterLenght = images.length - 1
+
+nextBtn.addEventListener('click', nextSlide)
+prevBtn.addEventListener('click', prevSlide)
+
+function nextSlide(){
+    images.scrollLeft += counter
+    if (counter + 1 > counterLenght) {
+        counter = 0
+    }else{
+        counter++
+    }
+}
+
+
+function prevSlide(){
+    images.scrollLeft -= counter
+    if (counter - 1 > counterLenght) {
+        counter = 0
+    }else{
+        counter--
+    }
+}
+
+// nextBtn.addEventListener('click', function(){
+//     if (counter + 1 > counterLenght) {
+//         counter = 0
+//     }else{
+//         counter = counter + 1
+//     }
+//     nextSlide()
+// })
